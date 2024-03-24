8664582f48209bac36ebbfd79958fa86c0e6bc36
Veren Villegas <verenvillegas@gmail.com>
2024-03-24 21:21:57 +1300

    Updated

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from src.classes import Database
 from src.classes import Utilities
 
+
 Utilities.clear_std_out()
 Utilities.print_welcome()
 
-args = ("Bed", 600, 2)
+
+
+args = ("Tree", -45, 2)
 Database.exec_proc("add_new_product", args)
+
+
+
 
 
 
--- src/classes/Database.py
+++ src/classes/Database.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def db_connect():
+    """
+    Establishes a connection to the database.
+    """
     conn = None
     try:
         conn = mysql.connector.connect(
@@ -19,18 +22,40 @@ def db_connect():
         return conn
 
 def db_disconnect(conn):
+    """
+    Closes the connection to the database.
+    """
     conn.commit()
     conn.close()
 
+
 def exec_proc(storedProc, args):
+    """
+    Executes a stored procedure from the database.
+
+    Parameters
+    -------
+    storedProc : str - The name of the stored procedure.
+    args : tuple - The arguments of the stored procedure.
+
+    """
     conn = db_connect()
     if(conn):
-        cursor = conn.cursor()
-        if(cursor):
-            if(args):
-                cursor.callproc(storedProc, args)
+        try:
+
+            cursor = conn.cursor()
+            if(cursor):
+                if(args):
+                    cursor.callproc(storedProc, args)
+                else:
+                    cursor.callproc(storedProc)
+        except mysql.connector.Error as e:
+            error_msg = str(e.msg)
+            if e.errno == 1644:  # SQLSTATE '45000'
+                print("Database Error:", error_msg)
             else:
-                cursor.callproc(storedProc)
-        db_disconnect(conn)
+                print("MySQL Error:", error_msg)
+        finally:    
+            db_disconnect(conn)
     else:
         print("Connection could not be established with database.")
